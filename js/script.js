ab6fee16d6e8ca19eb954fd7d643338a532e4e22
thiago-tsg <130875544+thiago-tsg@users.noreply.github.com>
2024-08-21 18:12:01 -0300

    f

--- js/script.js
+++ js/script.js
@@ -152,3 +152,52 @@ const initEvents = () => {
 };
 
 initEvents();
+
+
+// FORMULARIO CONTATO
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    password();
+    comparePassword();
+
+})
+
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 3)
+    {
+        setError(0);
+    } 
+    else
+    {
+        removeError(0);
+    }
+}
+
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else
+    {
+        removeError(1);
+    }
+}
